97713a09b2f801b0bb14a76eb0af29f930f2b025
Sally-A <Sally.Anderson@gmx.com>
2021-09-06 11:28:50 -0400

    Week5 Searching Algorithms

---
+++ week5/test.py
@@ -0,0 +1,7 @@
+streaming = ['netflix', 'hulu', 'disney+', 'appletv+']
+
+index = streaming.index('disney+')
+print('The index of disney+ is:', index)
+
+index = streaming.index('local')
+print('The index of local is:', index)
